5d4cc3f90bea1e0cc65eb5b5319de0e1b62e92d2
William Lebel <lebelwilliam@gmail.com>
2014-09-03 21:08:30 -0400

    Packet 0x44 added (WorldBorder)
    Signed-off-by: William Lebel <lebelwilliam@gmail.com>

---
+++ src/main/java/net/glowstone/net/codec/play/game/WorldBorderCodec.java
@@ -0,0 +1,72 @@
+package net.glowstone.net.codec.play.game;
+
+import com.flowpowered.networking.Codec;
+import com.flowpowered.networking.util.ByteBufUtils;
+import io.netty.buffer.ByteBuf;
+import net.glowstone.net.message.play.game.WorldBorderMessage;
+
+import java.io.IOException;
+//TODO: the longs need to be a VarLong
+public class WorldBorderCodec implements Codec<WorldBorderMessage> {
+    @Override
+    public WorldBorderMessage decode(ByteBuf buffer) throws IOException {
+        WorldBorderMessage worldBorderMessage = null;
+        int action = ByteBufUtils.readVarInt(buffer);
+        if (action == WorldBorderMessage.SET_SIZE) {
+            double radius = buffer.readDouble();
+            worldBorderMessage = new WorldBorderMessage(action, radius);
+        } else if (action == WorldBorderMessage.LERP_SIZE) {
+            double oldRadius = buffer.readDouble();
+            double newRadius = buffer.readDouble();
+            long speed = buffer.readLong();
+            worldBorderMessage = new WorldBorderMessage(action, oldRadius, newRadius, speed);
+        } else if (action == WorldBorderMessage.SET_CENTER) {
+            double x = buffer.readDouble();
+            double z = buffer.readDouble();
+            worldBorderMessage = new WorldBorderMessage(action, x, z);
+        } else if (action == WorldBorderMessage.INITIALIZE) {
+            double x = buffer.readDouble();
+            double z = buffer.readDouble();
+            double oldRadius = buffer.readDouble();
+            double newRadius = buffer.readDouble();
+            long speed = buffer.readLong();
+            int portalTeleportBoundary = ByteBufUtils.readVarInt(buffer);
+            int warningTime = ByteBufUtils.readVarInt(buffer);
+            int warningBlocks = ByteBufUtils.readVarInt(buffer);
+            worldBorderMessage = new WorldBorderMessage(action,x,z,oldRadius,newRadius,speed,portalTeleportBoundary,warningTime,warningBlocks);
+        } else if (action == WorldBorderMessage.SET_WARNING_TIME || action == WorldBorderMessage.SET_WARNING_BLOCKS) {
+            int warningTime = ByteBufUtils.readVarInt(buffer);
+            worldBorderMessage = new WorldBorderMessage(action, warningTime);
+        }
+        return worldBorderMessage;
+    }
+
+    @Override
+    public ByteBuf encode(ByteBuf buf, WorldBorderMessage message) throws IOException {
+        ByteBufUtils.writeVarInt(buf, message.getAction());
+        if (message.getAction() == WorldBorderMessage.SET_SIZE) {
+            buf.writeDouble(message.getRadius());
+        } else if (message.getAction() == WorldBorderMessage.LERP_SIZE) {
+            buf.writeDouble(message.getOldRadius());
+            buf.writeDouble(message.getNewRadius());
+            buf.writeLong(message.getSpeed());
+        } else if (message.getAction() == WorldBorderMessage.SET_CENTER) {
+            buf.writeDouble(message.getX());
+            buf.writeDouble(message.getZ());
+        } else if (message.getAction() == WorldBorderMessage.INITIALIZE) {
+            buf.writeDouble(message.getX());
+            buf.writeDouble(message.getZ());
+            buf.writeDouble(message.getOldRadius());
+            buf.writeDouble(message.getNewRadius());
+            buf.writeLong(message.getSpeed());
+            ByteBufUtils.writeVarInt(buf, message.getPortalTeleportBoundary());
+            ByteBufUtils.writeVarInt(buf, message.getWarningTime());
+            ByteBufUtils.writeVarInt(buf, message.getWarningBlocks());
+        } else if (message.getAction() == WorldBorderMessage.SET_WARNING_TIME) {
+            ByteBufUtils.writeVarInt(buf, message.getWarningTime());
+        } else if (message.getAction() == WorldBorderMessage.SET_WARNING_BLOCKS) {
+            ByteBufUtils.writeVarInt(buf, message.getWarningBlocks());
+        }
+        return buf;
+    }
+}
---
+++ src/main/java/net/glowstone/net/message/play/game/WorldBorderMessage.java
@@ -0,0 +1,153 @@
+package net.glowstone.net.message.play.game;
+
+import com.flowpowered.networking.Message;
+
+public class WorldBorderMessage implements Message {
+
+    public static final int SET_SIZE = 0, LERP_SIZE = 1, SET_CENTER = 2, INITIALIZE = 3, SET_WARNING_TIME = 4, SET_WARNING_BLOCKS = 5;
+    private int action;
+    private double radius;
+    private double oldRadius, newRadius;
+    private long speed;
+    private double x,z;
+    private int portalTeleportBoundary, warningTime, warningBlocks;
+
+    //SET_SIZE
+    public WorldBorderMessage(int action, double radius) {
+        this.action = action;
+        this.radius = radius;
+    }
+
+    //LERP_SIZE
+    public WorldBorderMessage(int action, double oldRadius, double newRadius, long speed) {
+        this.action = action;
+        this.oldRadius = oldRadius;
+        this.newRadius = newRadius;
+        this.speed = speed;
+    }
+
+    //SET_SENTER
+    public WorldBorderMessage(int action, double x, double z) {
+        this.action = action;
+        this.x = x;
+        this.z = z;
+    }
+
+    //INITIALIZE
+    public WorldBorderMessage(int action, double x, double z, double oldRadius, double newRadius, long speed, int portalTeleportBoundary, int warningTime, int warningBlocks) {
+        this.action = action;
+        this.x = x;
+        this.z = z;
+        this.oldRadius = oldRadius;
+        this.newRadius = newRadius;
+        this.speed = speed;
+        this.portalTeleportBoundary = portalTeleportBoundary;
+        this.warningTime = warningTime;
+        this.warningBlocks = warningBlocks;
+    }
+
+    //SET_WARNING_TIME
+    public WorldBorderMessage(int action, int warning) {
+        if (action == SET_WARNING_TIME) {
+            this.warningTime = warning;
+        } else if (action == SET_WARNING_BLOCKS) {
+            this.warningBlocks = warning;
+        }
+    }
+
+    public int getAction() {
+        return action;
+    }
+
+    public void setAction(int action) {
+        this.action = action;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getOldRadius() {
+        return oldRadius;
+    }
+
+    public void setOldRadius(double oldRadius) {
+        this.oldRadius = oldRadius;
+    }
+
+    public double getNewRadius() {
+        return newRadius;
+    }
+
+    public void setNewRadius(double newRadius) {
+        this.newRadius = newRadius;
+    }
+
+    public long getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(long speed) {
+        this.speed = speed;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public int getPortalTeleportBoundary() {
+        return portalTeleportBoundary;
+    }
+
+    public void setPortalTeleportBoundary(int portalTeleportBoundary) {
+        this.portalTeleportBoundary = portalTeleportBoundary;
+    }
+
+    public int getWarningTime() {
+        return warningTime;
+    }
+
+    public void setWarningTime(int warningTime) {
+        this.warningTime = warningTime;
+    }
+
+    public int getWarningBlocks() {
+        return warningBlocks;
+    }
+
+    public void setWarningBlocks(int warningBlocks) {
+        this.warningBlocks = warningBlocks;
+    }
+
+    @Override
+    public String toString() {
+        return "WorldBorderMessage{" +
+                "action=" + action +
+                ", radius=" + radius +
+                ", oldRadius=" + oldRadius +
+                ", newRadius=" + newRadius +
+                ", speed=" + speed +
+                ", x=" + x +
+                ", z=" + z +
+                ", portalTeleportBoundary=" + portalTeleportBoundary +
+                ", warningTime=" + warningTime +
+                ", warningBlocks=" + warningBlocks +
+                '}';
+    }
+}
